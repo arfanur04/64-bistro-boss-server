d3177022161463041a20f37341565a20b7adcb39
arfanur04 <arfanur04@gmail.com>
2024-05-30 14:26:08 +0600

    67-1 Module Introduction And Cart Icon Setup

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-	"mongodb+srv://<user>:<password>@cluster0.zxotz8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zxotz8q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,18 +20,52 @@ const client = new MongoClient(uri, {
 	},
 });
 
+const logger = (req, res, next) => {
+	try {
+		console.log("log info: ", req.method, req.url);
+		next();
+	} catch (error) {
+		console.error("error: ", error);
+		res.status(500).send({ message: "Internal Server Error" });
+	}
+};
+
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		// await client.connect();
+
+		const menuCollection = client.db("bistroDb").collection("menu");
+		const reviewsCollection = client.db("bistroDb").collection("reviews");
+
+		app.get("/menu", logger, async (req, res) => {
+			try {
+				const result = await menuCollection.find().toArray();
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
+		});
+
+		app.get("/reviews", logger, async (req, res) => {
+			try {
+				const result = await reviewsCollection.find().toArray();
+				res.send(result);
+			} catch (error) {
+				console.error("error: ", error);
+				res.status(500).send({ message: "Internal Server Error" });
+			}
+		});
+
 		// Send a ping to confirm a successful connection
-		await client.db("admin").command({ ping: 1 });
+		// await client.db("admin").command({ ping: 1 });
 		console.log(
 			"Pinged your deployment. You successfully connected to MongoDB!"
 		);
 	} finally {
 		// Ensures that the client will close when you finish/error
-		await client.close();
+		// await client.close();
 	}
 }
 run().catch(console.dir);
@@ -42,5 +75,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Server is lissed on port ${port}`);
+	console.log(`Server is listening on port ${port}`);
 });
